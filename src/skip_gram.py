f1fc2090e969e6a7f542005e05e65feaf4bf5ef5
Wbrta <ghlovezxcvbnm@vip.qq.com>
2018-05-16 11:02:29 +0800

    修复了 skip gram 一些小问题

--- src/skip_gram.py
+++ src/skip_gram.py
@@ -86,13 +86,14 @@ class SkipGram(object):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-C', '--corpus', type=str, help="语料文件路径", required=True)
-    parser.add_argument('-WS', '--window-size', type=int, help="目标词所需的上文词数", default=10)
-    parser.add_argument('-WES', '--word-embedding-size', type=int, help="词向量的大小", default=20)
-    parser.add_argument('-LR', '--learning-rate', type=float, help="学习速率", default=0.01)
-    parser.add_argument('-S', '--step', type=int, help="迭代次数", default=10000)
-    parser.add_argument('-BS', '--batch-size', type=int, help="批量生成数据的大小", default=50)
-    parser.add_argument('-SF', '--save-file', type=str, help="生成的词向量保存的位置", default="data/skip_gram.txt")
+    parser.add_argument('-C', '--corpus', type = str, help = "语料文件路径", required = True)
+    parser.add_argument('-VS', '--vocabulary_size', type = int, help = "字典大小", default = 10000)
+    parser.add_argument('-BS', '--batch-size', type = int, help = "批量生成数据的大小", default = 50)
+    parser.add_argument('-WS', '--window-size', type = int, help = "目标词所需的上下文词数", default = 10)
+    parser.add_argument('-WES', '--word-embedding-size', type = int, help = "词向量的大小", default = 300)
+    parser.add_argument('-LR', '--learning-rate', type = float, help = "学习速率", default = 0.01)
+    parser.add_argument('-E', '--epoches', type = int, help="迭代次数", default = 10000)
+    parser.add_argument('-SF', '--save-file', type = str, help="生成的词向量保存的位置", default="data/skip_gram.txt")
     args = parser.parse_args()
 
     sg = SkipGram(args.corpus, args.window_size, args.word_embedding_size, args.learning_rate, args.step, args.batch_size, args.save_file)
